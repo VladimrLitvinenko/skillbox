fcc4c997d6af1a67846c562065c717698e7ac87b
= <vladimirvitvinenko@gmail.com>
2022-04-17 21:45:54 +0300

    changed

--- git_module/first.py
+++ git_module/first.py
@@ -1,4 +1,4 @@
 print("i was updated within dev branch")
 print("i was updated within dev branch 2 updated")
-print("Cancel button")
+print("Cancel button updated")
 print("Выберите пункт доставки")
